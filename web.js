049d2b6707b3811816dc802bf5300a353638c7dd
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2013-07-09 21:06:57 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs=require('fs')
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  contents=fs.readFileSync('index.html');
+      
+  response.send(contents.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
